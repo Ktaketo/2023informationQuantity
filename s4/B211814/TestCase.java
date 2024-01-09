94e901a06147f0423fadedc83beaae29dc43b037
KITAHAMA Taketo <t211814@xdev06.edu.tut.ac.jp>
2024-01-09 13:12:24 +0900

    Message-20231219

--- s4/B211814/TestCase.java
+++ s4/B211814/TestCase.java
@@ -46,6 +46,43 @@ public class TestCase {
 	    assert freq == 4: "Hi Ho Hi Ho, H: " + freq;
 	    // Write your testCase here
 
+            // week2, Step7
+	    // Test case for ArrayIndexOutOfBoundsException in frequency() method
+	    myObject = new Frequencer();
+	    myObject.setSpace("abc".getBytes());
+	    myObject.setTarget("abcde".getBytes()); // Target length is greater than space length
+	    try {
+		freq = myObject.frequency();
+	    } catch (ArrayIndexOutOfBoundsException e){
+		System.out.println("ArrayIndexOutOfBoundsException occurred in Frequencer Object");
+	    }
+
+	    /* Week1, Step13
+	           myObject = new Frequencer();
+		       myObject.setSpace("ABABAABA".getBytes());
+		           myObject.setTarget("ABA".getBytes());
+			       freq = myObject.frequency();
+			           assert freq == 3: "ABABAABA, ABA: " + freq; // 一部が重複する文字列
+
+				       myObject = new Frequencer();
+				           myObject.setSpace("A".getBytes());
+					       myObject.setTarget("AB".getBytes());
+					           freq = myObject.frequency();
+						   assert freq == 0: "A, AB: " + freq; // Targetの方がSpaceより長い  
+            */
+	    
+	    /* Week1, Step14
+	           myObject = new Frequencer();
+		       myObject.setSpace("Hi Ho Hi Ho".getBytes());
+		           freq = myObject.frequency();
+			       assert freq == -1: "TARGET is not set or TARGET's length is zero"; // Targetが初期化されていないときは-1を返す 
+
+			           myObject = new Frequencer();
+				       myObject.setTarget("H".getBytes());
+				           freq = myObject.frequency();
+					       assert freq == 0: "SPACE is not set or Space's length is zero"; // Spaceが初期化されていないときは0を返す
+	    */
+
 
 	}
 	catch(Exception e) {
@@ -78,5 +115,5 @@ public class TestCase {
 	}
         if(success) { System.out.println("TestCase OK"); } 
     }
-}	    
-	    
+}    
+    
